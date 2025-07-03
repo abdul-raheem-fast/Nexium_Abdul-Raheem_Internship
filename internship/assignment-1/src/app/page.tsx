6fb28ce1d59153279e13983773f862965aa8f220
Abdul Raheem <abdulraheemghauri@gmail.com>
2025-07-03 22:03:34 +0500

    feat: add ShadCN UI button to home page

--- internship/assignment-1/src/app/page.tsx
+++ internship/assignment-1/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -24,6 +25,9 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
+
+        {/* ShadCN UI Button added here */}
+        <Button>Click me</Button>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
